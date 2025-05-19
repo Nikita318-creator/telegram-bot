d514a0c59fce694f45ba512edbb54cb481abf238
Nikita318-creator <nikuvar77@gmail.com>
2025-05-19 22:35:28 +0300

    Add test

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Yo! I'm alive.")
+    await update.message.reply_text("Yo! I'm GAY.")
 
 def main():
     token = os.getenv("TELEGRAM_BOT_TOKEN")
